5e9a98f872479b5132d989e321ef08c5f0f0d6a2
sainath9392 <sainathduvvuri03@gmail.com>
2025-05-28 16:12:50 +0530

    fix: Correct import case for Item component in PopularProducts

--- frontend/Outfiro/src/components/PopularProducts.jsx
+++ frontend/Outfiro/src/components/PopularProducts.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
-import Item from "./item";
+import Item from "./Item";
 
 const PopularProducts = () => {
   const { products } = useContext(ShopContext);
