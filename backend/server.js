30c792539739c10f943a6ca0916cfb43be2f0795
sainath9392 <sainathduvvuri03@gmail.com>
2025-05-29 10:49:58 +0530

    feat: Add Cloudinary configuration and connection setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 //App config
 const app = express();
@@ -11,6 +12,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 connectDB()
+connectCloudinary()
 
 
 //Api endpoints
