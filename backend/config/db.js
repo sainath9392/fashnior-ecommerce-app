81c70098382282b82680d33156874acf892598bd
sainath9392 <sainathduvvuri03@gmail.com>
2025-05-29 10:18:15 +0530

    feat: Add database connection setup with mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const connectDB = async () => {
+    try {
+        //establish connection
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('✅ Database connected')
+    } catch (error) {
+        console.log("❌ Database connection failed "+error.message)
+    }
+}
+
+export default connectDB
