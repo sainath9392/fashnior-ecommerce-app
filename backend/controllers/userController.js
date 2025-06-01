c36deada9001170ac697291e984076c1b7e84654
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-01 20:43:17 +0530

    feat: Enhance user registration functionality with validation and token generation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,60 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+// Function for creating token
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
 //controller function for user login
-const loginUser = async (req,res) =>{
-    
-}
+const loginUser = async (req, res) => {};
 
 //controller function for user register
-const registerUser = async (req,res) =>{
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    //checking if user already exists
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    //validate password and checking password strength
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "please enter a strong password",
+      });
+    }
 
-}
+    //hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name: name,
+      email: email,
+      password: hashedpassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error)
+    res.json({success:false, message:error.message})
+  }
+};
 
 //controller function for user admin login
-const adminLoginUser = async (req,res) =>{
+const adminLoginUser = async (req, res) => {};
 
-}
-
-export {loginUser,adminLoginUser,registerUser}
+export { loginUser, adminLoginUser, registerUser };
