6030921ec7c1b9f24be1d3cff551e832052e5001
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-02 10:23:06 +0530

    feat: Implement product controller functions for adding, removing, and retrieving products

---
+++ backend/controllers/productController.js
@@ -0,0 +1,18 @@
+//Controller function for adding a project
+const addproduct = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+//Controller function to remove product
+const removeProduct = async (req, res) => {};
+
+//Controller function for single product details
+const singleProduct = async (req, res) => {};
+
+const listProduct = async (Request, res) => {};
+
+export { addproduct, listProduct, removeProduct, singleProduct };
