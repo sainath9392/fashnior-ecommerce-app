f8514b18cea771f9bdad83c946aa03a51dcb9034
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-03 22:13:10 +0530

    feat: Update product route to include image upload functionality

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,11 +4,12 @@ import {
   listProduct,
   removeProduct,
   singleProduct,
-} from "../controllers/productController";
+} from "../controllers/productController.js";
+import upload from "../middleware/multer.js";
 
 const productRouter = express.Router();
 
-productRouter.post("/add", addproduct);
+productRouter.post("/add",upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount1:1},{name:'image3',maxCount:1},{name:"image4",maxCount:1}]), addproduct);
 productRouter.delete("/remove", removeProduct);
 productRouter.get("/single", singleProduct);
 productRouter.get("list", listProduct);
