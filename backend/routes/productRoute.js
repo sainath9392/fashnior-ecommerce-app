676983ade9c2d8f9af21c492c0cc233968ea20c2
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-05 21:33:45 +0530

    feat: Add admin authentication to add and remove product routes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,12 +6,23 @@ import {
   singleProduct,
 } from "../controllers/productController.js";
 import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router();
 
-productRouter.post("/add",upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount1:1},{name:'image3',maxCount:1},{name:"image4",maxCount:1}]), addproduct);
-productRouter.post("/remove", removeProduct);
+productRouter.post(
+  "/add",
+  adminAuth,
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount1: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addproduct
+);
+productRouter.post("/remove", adminAuth, removeProduct);
 productRouter.post("/single", singleProduct);
 productRouter.get("/list", listProduct);
 
-export default productRouter
+export default productRouter;
