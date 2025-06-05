1d7bedce58584b3ea19ea88616cc4cc55d0a8a04
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-05 18:06:19 +0530

    fix: Change remove product route method from DELETE to POST

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -10,7 +10,7 @@ import upload from "../middleware/multer.js";
 const productRouter = express.Router();
 
 productRouter.post("/add",upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount1:1},{name:'image3',maxCount:1},{name:"image4",maxCount:1}]), addproduct);
-productRouter.delete("/remove", removeProduct);
+productRouter.post("/remove", removeProduct);
 productRouter.post("/single", singleProduct);
 productRouter.get("/list", listProduct);
 
