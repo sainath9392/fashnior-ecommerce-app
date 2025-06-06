4a71e7e17147c2f327b8fdf4190f4d4c1f970210
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-06 16:37:08 +0530

    refactor: Enhance Slidebar component structure and styling for better layout and navigation

--- admin/admin/src/components/Slidebar.jsx
+++ admin/admin/src/components/Slidebar.jsx
@@ -1,31 +1,62 @@
-import React from 'react'
-import { Link,NavLink } from 'react-router-dom'
-import {FaSquarePlus, } from 'react-icons/fa6'
-import {MdFactCheck} from "react-icons/md"
-import {BiLogOut} from "react-icons/bi"
-import {FaListAlt} from 'react-icons/fa'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FaSquarePlus } from "react-icons/fa6";
+import { MdFactCheck } from "react-icons/md";
+import { BiLogOut } from "react-icons/bi";
+import { FaListAlt } from "react-icons/fa";
 
 const Slidebar = () => {
   return (
-    <div>
-        <div>
-            {/* logo */}
-            <Link to={'/'}>Fashni<span className=''>o</span>r</Link>
-            <div>
-                <NavLink>
-                    <FaSquarePlus />
-                    Add Item
-                </NavLink>
-                <div>
-                    <button>
-                        <BiLogOut />
-                        <div>Logout</div>
-                    </button>
-                </div>
-            </div>
+    <div className="max-sm:flexCenter max-xs:pb-3 rounded bg-white pb-3 sm:w-1/5 sm:min-h-csreen">
+      <div className="flex flex-col gap-y-6 max-sm:items-center sm:flex-col pt-4 sm:pt-14">
+        {/* logo */}
+        <Link className="bold-22 xl:bold-32 sm:pl-2 lg:pl-12" to={"/"}>
+          Fashni<span className=" text-secondary">o</span>r
+        </Link>
+        <div className="flex sm:flex-col gap-x-5 gap-y-8 sm:pt-10">
+          <NavLink
+            to={"/"}
+            className={({ isActive }) =>
+              isActive
+                ? "active-link"
+                : "flexStart gap-x-2 sm:pl-12 p-5 medium-15 cursor-pointer h-10 rounded-xl"
+            }
+          >
+            <FaSquarePlus />
+            <div className="hidden lg:flex">Add Item</div>
+          </NavLink>
+          <NavLink
+            to={"/list"}
+            className={({ isActive }) =>
+              isActive
+                ? "active-link"
+                : "flexStart gap-x-2 sm:pl-12 p-5 medium-15 cursor-pointer h-10 rounded-xl"
+            }
+          >
+            <FaListAlt />
+            <div className="hidden lg:flex">List</div>
+          </NavLink>
+          <NavLink
+            to={"/orders"}
+            className={({ isActive }) =>
+              isActive
+                ? "active-link"
+                : "flexStart gap-x-2 sm:pl-12 p-5 medium-15 cursor-pointer h-10 rounded-xl"
+            }
+          >
+            <MdFactCheck />
+            <div className="hidden lg:flex">Orders</div>
+          </NavLink>
+          <div>
+            <button>
+              <BiLogOut />
+              <div>Logout</div>
+            </button>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Slidebar
+export default Slidebar;
