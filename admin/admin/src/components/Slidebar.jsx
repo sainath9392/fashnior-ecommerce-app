ca13880931fb015fc637e01fdad67cd7fefb510d
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-09 15:24:48 +0530

    fix: Update Slidebar component to improve layout with sticky positioning

--- admin/admin/src/components/Slidebar.jsx
+++ admin/admin/src/components/Slidebar.jsx
@@ -7,7 +7,7 @@ import { FaListAlt } from "react-icons/fa";
 
 const Slidebar = ({setToken}) => {
   return (
-    <div className="max-sm:flexCenter max-xs:pb-3 rounded bg-white pb-3 sm:w-1/5 sm:min-h-screen ">
+    <div className="max-sm:flexCenter max-xs:pb-3 rounded bg-white pb-3 sm:w-1/5 sm:max-h-screen sm:sticky sm:top-0 sm:left-0">
       <div className="flex flex-col gap-y-6 max-sm:items-center sm:flex-col pt-4 sm:pt-14">
         {/* logo */}
         <Link className="bold-22 xl:bold-32 sm:pl-2 lg:pl-12" to={"/"}>
