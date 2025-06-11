fd4238300f52354136c8f670232d50c95254611c
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-11 23:43:09 +0530

    feat: Add order routing to the server for order management

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 //App config
 const app = express();
@@ -21,6 +22,7 @@ connectCloudinary();
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 //Api endpoints
 app.get("/", (req, res) => {
