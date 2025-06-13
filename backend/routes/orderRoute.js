0eda018a217549a37060a8ce6b4eb20a06075264
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-13 20:54:37 +0530

    refactor: Clean up orderRouter by removing unused routes and fixing formatting

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -2,24 +2,19 @@ import express from "express";
 import {
   allOrders,
   palceOrder,
-  palceOrderStripe,
   updateStatus,
   userOrders,
-  verifyStripe,
 } from "../controllers/orderController.js";
 import adminAuth from "../middleware/adminAuth.js";
-import authUser from "../middleware/auth.js"
+import authUser from "../middleware/auth.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/list", adminAuth, allOrders);
 orderRouter.post("/status", adminAuth, updateStatus);
 
-orderRouter.post("/place", authUser,palceOrder);
-orderRouter.post("/stripe",authUser, palceOrderStripe);
+orderRouter.post("/place", authUser, palceOrder);
 
-orderRouter.post("/userorders",authUser, userOrders);
+orderRouter.post("/userorders", authUser, userOrders);
 
-orderRouter.post("/verifystripe",authUser,verifyStripe)
-
-export default orderRouter
+export default orderRouter;
